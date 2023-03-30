69b478bdf79919124342ff06872cfd2a1b106e52
ZeroWave_ <36341766+ZeroWave022@users.noreply.github.com>
2023-03-30 12:04:36 +0200

    feat(dashboard): Add play/resume button

---
+++ musicvision/static/js/dashboard.js
@@ -0,0 +1,31 @@
+const icons = {
+    play: "circle-play-solid.svg",
+    pause: "circle-pause-solid.svg"
+}
+
+async function play_button_event() {
+    let res = await fetch("./api/toggle_player", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    resContent = await res.json();
+
+    if (resContent.status != "success") {
+        return;
+    }
+
+    image = document.querySelector("#track-info img");
+    srcSplit = image.src.split("/");
+
+    if (resContent.isPlaying) {
+        srcSplit[srcSplit.length - 1] = icons.pause;
+    }
+    else {
+        srcSplit[srcSplit.length - 1] = icons.play;
+    }
+
+    image.src = srcSplit.join("/");
+}
