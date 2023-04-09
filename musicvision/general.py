10e037e360e1c8d60815f9d8d66154858fff1bf1
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2023-04-09 23:17:34 +0200

    fix: Check for token refresh on all pages

--- musicvision/general.py
+++ musicvision/general.py
@@ -1,56 +1,64 @@
 import time
+import logging
 from flask import Blueprint, render_template, redirect, session
 from musicvision import spotify_app
-from musicvision.spotify import SpotifyUser
 from musicvision.db import query_db, fetch_db
 
 general_bp = Blueprint("general", __name__)
 
 
-@general_bp.route("/")
-def index():
+@general_bp.before_app_request
+def check_token_refresh():
     user = session.get("user")
 
     if not user:
-        return render_template("index.html")
+        return
+
+    try:
+        expires_at = user["expires_at"]
+    except:
+        logging.warning(
+            "User dict for %s is corrupted. Key 'expires_at' is not available. Resetting.",
+            user["id"],
+        )
+        return session.clear()
+
+    # Check if token refresh is needed
+    if not expires_at < time.time():
+        return
 
     current_user = fetch_db(
         "SELECT * FROM users WHERE access_token = %s", "one", [user["access_token"]]
     )
 
-    token_refresh_required = current_user["expires_at"] < time.time()
+    refreshed_info = spotify_app.refresh_token(current_user["refresh_token"])
 
-    if token_refresh_required:
-        refreshed = spotify_app.refresh_token(current_user["refresh_token"])
+    # Add new info to current_user dict, which later will be sent to the database
+    current_user["old_token"] = current_user["access_token"]
+    current_user["access_token"] = refreshed_info["access_token"]
+    current_user["expires_at"] = round(time.time() + refreshed_info["expires_in"])
 
-        current_user["old_token"] = current_user["access_token"]
-        current_user["access_token"] = refreshed["access_token"]
-        current_user["expires_at"] = round(time.time() + refreshed["expires_in"])
+    if "refresh_token" in refreshed_info:
+        current_user["refresh_token"] = refreshed_info["refresh_token"]
 
-        new_refresh_token = refreshed.get("refresh_token")
-        if new_refresh_token:
-            current_user["refresh_token"] = refreshed["refresh_token"]
-
-        query_db(
-            """
-            UPDATE users
-            SET access_token=%(access_token)s, refresh_token=%(refresh_token)s, expires_at=%(expires_at)s
-            WHERE access_token=%(old_token)s
-            """,
-            current_user,
-        )
-
-    user = SpotifyUser(current_user["access_token"])
-    profile = user.get_profile()
-
-    if token_refresh_required:
-        session["user"] = current_user
-
-    return render_template(
-        "index.html",
-        name=profile["display_name"],
-        followers=profile["followers"]["total"],
+    query_db(
+        """
+        UPDATE users
+        SET access_token=%(access_token)s, refresh_token=%(refresh_token)s, expires_at=%(expires_at)s
+        WHERE access_token=%(old_token)s
+        """,
+        current_user,
     )
+
+    session.clear()
+
+    del current_user["old_token"]
+    session["user"] = current_user
+
+
+@general_bp.route("/")
+def index():
+    return render_template("index.html")
 
 
 @general_bp.route("/features")
