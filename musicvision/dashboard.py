914c2c88a4f837d7b2df67d139043c9857d0b349
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2023-04-13 22:35:24 +0200

    feat: Add filtering and pages to top pages

--- musicvision/dashboard.py
+++ musicvision/dashboard.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, abort, session
+from flask import Blueprint, render_template, redirect, url_for, abort, session, request
 from musicvision.spotify import SpotifyUser
 from musicvision.db import fetch_db, query_db
 
@@ -52,8 +52,21 @@ def top_artists():
     if not session.get("user"):
         return redirect(url_for("auth.login"))
 
+    filter = request.args.get("f")
+    page = request.args.get("p", type=int)
+
+    # If a wrong filter is specified, use the default
+    if filter not in ["long_term", "medium_term", "short_term"]:
+        filter = "long_term"
+
+    # page must be a positive integer under 5 (max 50 items from API)
+    if not isinstance(page, int) or page > 5 or page < 1:
+        page = 1
+
     user = SpotifyUser(session["user"]["access_token"])
-    artists_raw = user.get_top("artists", "medium_term", 25)
+
+    # Decrement page by one to get correct offset
+    artists_raw = user.get_top("artists", filter, 10, (page - 1) * 10)
     artists = artists_raw["items"]
 
     for artist in artists:
@@ -65,7 +78,10 @@ def top_artists():
     )
 
     return render_template(
-        "dashboard/top-artists.html", artists=enumerate(artists_sorted)
+        "dashboard/top-artists.html",
+        artists=enumerate(artists_sorted),
+        filter=filter,
+        page=page,
     )
 
 
@@ -74,13 +90,27 @@ def top_tracks():
     if not session.get("user"):
         return redirect(url_for("auth.login"))
 
-    user = SpotifyUser(session["user"]["access_token"])
-    tracks_raw = user.get_top("tracks", "medium_term", 25)
+    filter = request.args.get("f")
+    page = request.args.get("p", type=int)
 
+    # If a wrong filter is specified, use the default
+    if filter not in ["long_term", "medium_term", "short_term"]:
+        filter = "long_term"
+
+    # page must be a positive integer under 5 (max 50 items from API)
+    if not isinstance(page, int) or page > 5 or page < 1:
+        page = 1
+
+    user = SpotifyUser(session["user"]["access_token"])
+
+    # Decrement page by one to get correct offset
+    tracks_raw = user.get_top("tracks", filter, 10, (page - 1) * 10)
     tracks = tracks_raw["items"]
     tracks = sorted(tracks, key=lambda i: i["popularity"], reverse=True)
 
-    return render_template("dashboard/top-tracks.html", tracks=enumerate(tracks))
+    return render_template(
+        "dashboard/top-tracks.html", tracks=enumerate(tracks), filter=filter, page=page
+    )
 
 
 @dashboard_bp.get("delete-account")
